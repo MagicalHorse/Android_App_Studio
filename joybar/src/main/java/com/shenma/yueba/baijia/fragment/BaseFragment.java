245d6f0ad511b8de3e5b56cc45115ba73ba5bc45
gyj <gaoyj@joybar.com.cn>
2015-12-18 16:18:25 +0800

    界面  细节优化

--- joybar/src/main/java/com/shenma/yueba/baijia/fragment/BaseFragment.java
+++ joybar/src/main/java/com/shenma/yueba/baijia/fragment/BaseFragment.java
@@ -9,70 +9,67 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lidroid.xutils.HttpUtils;
-import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import com.shenma.yueba.R;
 import com.shenma.yueba.constants.Constants;
-import com.shenma.yueba.util.CustomProgressDialog;
 
 import java.util.ArrayList;
 
-import de.greenrobot.event.EventBus;
-
 public class BaseFragment extends Fragment {
 
-	public BaseFragment()
-	{
+    public BaseFragment() {
 
-	}
-
-
-	private HttpUtils httpUtils;
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		httpUtils = new HttpUtils();
-		super.onCreate(savedInstanceState);
-
-	}
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return super.onCreateView(inflater, container, savedInstanceState);
-	}
-	
-	
-	protected HttpUtils getHttpUtils(){
-		if(httpUtils == null){
-			httpUtils = new HttpUtils();
-		}else{
-			return httpUtils;
-		}
-		return httpUtils;
-	}
+    }
 
 
-	protected void setCursorAndText(int index,ArrayList<ImageView> cursorImageList,ArrayList<TextView> titleTextList) {
-		for (int i = 0; i < cursorImageList.size(); i++) {
-			if (i != index) {
-				cursorImageList.get(i).setVisibility(View.INVISIBLE);
-			} else {
-				cursorImageList.get(i).setVisibility(View.VISIBLE);
-			}
-		}
-		for (int j = 0; j < titleTextList.size(); j++) {
-			if (j != index) {
-				titleTextList.get(j).setTextSize(Constants.title_text_normal_size);
-				titleTextList.get(j).setTextColor(getResources().getColor(R.color.text_gray_color));
-			} else {
-				titleTextList.get(j).setTextSize(Constants.title_text_selected_size);
-				titleTextList.get(j).setTextColor(getResources().getColor(R.color.main_color));
-			}
-		}
-	}
+    private HttpUtils httpUtils;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        httpUtils = new HttpUtils();
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
+    protected HttpUtils getHttpUtils() {
+        if (httpUtils == null) {
+            httpUtils = new HttpUtils();
+        } else {
+            return httpUtils;
+        }
+        return httpUtils;
+    }
 
-	}
+
+    protected void setCursorAndText(int index, ArrayList<ImageView> cursorImageList, ArrayList<TextView> titleTextList) {
+        for (int i = 0; i < cursorImageList.size(); i++) {
+            if (i != index) {
+                cursorImageList.get(i).setVisibility(View.INVISIBLE);
+            } else {
+                cursorImageList.get(i).setVisibility(View.VISIBLE);
+            }
+        }
+        for (int j = 0; j < titleTextList.size(); j++) {
+            if (j != index) {
+                titleTextList.get(j).setTextSize(Constants.title_text_normal_size);
+                titleTextList.get(j).setTextColor(getResources().getColor(R.color.text_gray_color));
+            } else {
+                titleTextList.get(j).setTextSize(Constants.title_text_selected_size);
+                titleTextList.get(j).setTextColor(getResources().getColor(R.color.main_color));
+            }
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
 }
